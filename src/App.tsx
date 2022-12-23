25c9c8de5824f3380557ef3a61ef044209045a18
guoyanlun08 <970789443@qq.com>
2022-12-23 22:51:35 +0800

    feat: 项目初步搭建

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 'sss';
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,13 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          11 Learn React
         </a>
       </header>
     </div>
