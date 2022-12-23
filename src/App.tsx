b2c12688edb302657fc1a40d1f8920b5cedd3fc5
guoyanlun08 <970789443@qq.com>
2022-12-24 00:15:08 +0800

    feat: 引入 sass,  重写webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const a = 'sss';
+import { EveryDay } from './views/EveryDay';
 
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          11 Learn React
-        </a>
-      </header>
+      <EveryDay />
     </div>
   );
 }
---
+++ src/views/EveryDay/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from './index.module.scss';
+
+export function EveryDay() {
+  return <div className={style['every-day']}>111</div>;
+}
