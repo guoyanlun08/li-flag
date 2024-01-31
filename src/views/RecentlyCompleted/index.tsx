bb122eb64da0d124515a8d3b2aa15f752c2e3850
v_yanlunguo <v_yanlunguo@tencent.com>
2024-01-31 19:09:31 +0800

    feat: 【近期完成】构建

---
+++ src/views/RecentlyCompleted/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function RecentlyCompleted() {
+  return (
+    <div>
+      {/* 标题 近x天 */}
+      {/* 能切换显示今日已完成？？？ */}
+      {/* 选择器也可以手动输入筛选时间  */}
+      {/* switch 分页非分页 */}
+      {/* 已完成列表 */}
+    </div>
+  );
+}
+
+export default RecentlyCompleted;
