7c75f2a41b6a11aad8439170c5c90f03484aa75d
huangzhiquan <643029389@qq.com>
2024-01-31 23:55:04 +0800

    refactor:修改函数名

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,20 @@ const { Sider, Header, Content } = Layout;
 function App() {
   const [close, setClose] = useState(true);
   const [switchList, setSwitchList] = useState(false);
-  // todo: 建议修改函数名, 本意是去 折叠、展开
-  const getClose = (isClose: boolean) => {
+  const expAndCloseMenu = (isClose: boolean) => {
     setClose(isClose);
   };
-  // todo: 建议修改函数名, 本意是去切换 list 和 card 状态
-  const getSwitch = (isList: boolean) => {
+  const switchToList = (isList: boolean) => {
     setSwitchList(isList);
   };
   return (
     <Layout style={{ width: '100%', height: '100%' }}>
       <Sider collapsed={close}>
-        <SiderMenu getClose={getClose} />
+        <SiderMenu getClose={expAndCloseMenu} />
       </Sider>
       <Layout>
         <Header style={{ background: 'white' }}>
-          <ToolsBar getSwitch={getSwitch} />
+          <ToolsBar getSwitch={switchToList} />
         </Header>
         <Content className="hiddenScroll" style={{ margin: '10px' }}>
           <EveryDay switchList={switchList} />
