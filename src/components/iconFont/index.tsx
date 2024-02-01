3b96820b5f78eeb4bb941ca6a3a57fa31a731b97
I can <970789443@qq.com>
2024-02-01 14:36:12 +0800

    feat:  IconFont 组件 (SuperB 必看)

--- src/components/iconFont/index.tsx
+++ src/components/iconFont/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { createFromIconfontCN } from '@ant-design/icons';
 import { Space } from 'antd';
-const iconUrl = [
-    '//at.alicdn.com/t/c/font_4429243_mr9nhpea77g.js',
-]
+const iconUrl = ['//at.alicdn.com/t/c/font_4429243_13a7lsv8ljt.js'];
 const CreateIcon = createFromIconfontCN({
-    scriptUrl: iconUrl,
+  scriptUrl: iconUrl
 });
 interface IProps {
-    name: string;
+  name: string;
 }
 
 export default function IconFont(props: IProps) {
-    return (
-        <Space>
-            <CreateIcon type={props.name} style={{fontSize: '30px'}}/>
-        </Space>
-    )
+  return (
+    <Space>
+      {/* todo: fontSize需要传参定义，不要写死 */}
+      {/* Readme 简单补充一下图标库信息 */}
+      <CreateIcon type={props.name} style={{ fontSize: '30px' }} />
+    </Space>
+  );
 }
