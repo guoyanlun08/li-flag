504373715c1575f95e3cfff69329be94ea9d8f9e
I can <970789443@qq.com>
2024-02-02 18:25:02 +0800

    feat: 优美命名

--- src/views/RecentlyCompleted/comp/List.tsx
+++ src/views/RecentlyCompleted/comp/List.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { Styled_ListBox } from '../Styles';
+import { Styled_List } from '../Styles';
 
 /** 近期模块 —— completed列表 */
 function ListBox() {
   return (
-    <Styled_ListBox>
+    <Styled_List>
       <div>1</div>
       <div>2</div>
       <div>3</div>
-    </Styled_ListBox>
+    </Styled_List>
   );
 }
 
