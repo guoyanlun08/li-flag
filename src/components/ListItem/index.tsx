fd143d4a81f1d3b52d6c66670a644437e335b33f
huangzhiquan <643029389@qq.com>
2024-03-01 00:19:23 +0800

    feat: 增加右键菜单(删除选项)

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Checkbox } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
+import { Menu, Item, useContextMenu, ItemParams } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.css';
 
 import { useAppSelector, useAppDispatch } from '@/app/hooks';
-import { toggleItemCompletedStatus, setSelectedId } from '@/features/todo/todoSlice';
+import { toggleItemCompletedStatus, setSelectedId, getTodoListThunk } from '@/features/todo/todoSlice';
+import api from '@/utils/httpRequest';
 import { EveryDayContext } from '@/views/EveryDay';
 
 import { Styled_Item, Styled_ItemContent } from './Styles';
@@ -22,6 +25,10 @@ export function ListItem(props: PropsType) {
   const context = useContext(EveryDayContext);
   const dispatch = useAppDispatch();
   const todoState = useAppSelector((store) => store.todo);
+  const MENU_ID = 'rightMenu';
+  const { show } = useContextMenu({
+    id: MENU_ID
+  });
 
   const { moduleId, id, editable, value, index, completed, dragHandle } = props;
   const [isHover, setIsHover] = useState(false);
@@ -46,24 +53,46 @@ export function ListItem(props: PropsType) {
     if (context.dragStatus) return;
     setIsHover(false);
   };
+  const displayMenu = (e: React.MouseEvent) => {
+    show({
+      event: e
+    });
+  };
+  const deleteItemClick = async ({ event, props, triggerEvent, data }: ItemParams) => {
+    console.log(data);
+    // 每个模块都加数据，点击删除看打印的数据都是同一个模块，哪里传的参数可能不对
+    // const res = await api.post('/todoItem/deleteTodoItemById', { id });
+    // console.log(res);
+    // if (res && res.code === 0) {
+    //   dispatch(getTodoListThunk());
+    // }
+  };
 
   return (
-    <Styled_Item
-      selected={isSelected}
-      onMouseDown={selectItemFn}
-      onMouseEnter={mouseEnterItemFn}
-      onMouseLeave={mouseLeaveItemFn}
-      onDoubleClick={(e) => e.stopPropagation()}>
-      <MenuOutlined style={{ display: editable && isHover ? 'block' : 'none' }} className="drag-handle" {...dragHandle} />
-      <Checkbox
-        checked={Boolean(completed)}
-        disabled={!editable}
-        onChange={() => dispatch(toggleItemCompletedStatus({ moduleId: moduleId, itemIndex: index }))}
-      />
-      <Styled_ItemContent selected={isSelected}>
-        {/* todo: 展示 value的 text需要处理 */}
-        {editable ? <EditNode selected={isSelected} /> : <div></div>}
-      </Styled_ItemContent>
-    </Styled_Item>
+    <>
+      <Styled_Item
+        selected={isSelected}
+        onMouseDown={selectItemFn}
+        onMouseEnter={mouseEnterItemFn}
+        onMouseLeave={mouseLeaveItemFn}
+        onDoubleClick={(e) => e.stopPropagation()}
+        onContextMenu={displayMenu}>
+        <MenuOutlined style={{ display: editable && isHover ? 'block' : 'none' }} className="drag-handle" {...dragHandle} />
+        <Checkbox
+          checked={Boolean(completed)}
+          disabled={!editable}
+          onChange={() => dispatch(toggleItemCompletedStatus({ moduleId: moduleId, itemIndex: index }))}
+        />
+        <Styled_ItemContent selected={isSelected}>
+          {/* todo: 展示 value的 text需要处理 */}
+          {editable ? <EditNode selected={isSelected} /> : <div></div>}
+        </Styled_ItemContent>
+      </Styled_Item>
+      <Menu id={MENU_ID} animation="fade">
+        <Item onClick={deleteItemClick} data={{ moduleId, id }}>
+          删除
+        </Item>
+      </Menu>
+    </>
   );
 }
