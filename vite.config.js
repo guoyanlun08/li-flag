617d6526f84d063fdba9e52f1493678bd60e9085
allen哥 <970789443@example.com>
2025-09-12 01:00:30 +0800

    feat: 代码分割优化

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build'
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          plugins: [visualizer()],
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // 将node_modules中的依赖分组
+              if (id.includes('react')) {
+                return 'vendor-react';
+              }
+              if (id.includes('antd')) {
+                return 'vendor-antd';
+              }
+              if (id.includes('axios')) {
+                return 'vendor-axios';
+              }
+              if (id.includes('slate') || id.includes('slate-react')) {
+                return 'vendor-slate';
+              }
+              return 'vendor'; // 其他依赖
+            }
+          }
+        }
+      }
     },
     plugins: [react()],
     resolve: {
