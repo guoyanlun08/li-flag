6cc9107c4c6b511888e430517613305065469897
huangzhiquan <643029389@qq.com>
2025-10-12 00:30:31 +0800

    fix: 调整表格空状态元素层级

--- src/views/TeamFlag/TeamFlagDetail/components/TableRow.tsx
+++ src/views/TeamFlag/TeamFlagDetail/components/TableRow.tsx
@@ -24,15 +24,16 @@ export const DragHandle: React.FC<{ isLock: boolean }> = ({ isLock }) => {
 };
 
 const Row: React.FC<RowProps> = (props) => {
+  const id = props['data-row-key'];
   const { attributes, listeners, setNodeRef, setActivatorNodeRef, transform, transition, isDragging } = useSortable({
-    id: props['data-row-key']
+    id
   });
 
   const style: React.CSSProperties = {
     ...props.style,
     transform: CSS.Translate.toString(transform),
     transition,
-    ...(isDragging ? { position: 'relative', zIndex: 9999 } : {})
+    ...(isDragging && id ? { position: 'relative', zIndex: 9999 } : {})
   };
 
   const contextValue = useMemo<RowContextProps>(() => ({ setActivatorNodeRef, listeners }), [setActivatorNodeRef, listeners]);
